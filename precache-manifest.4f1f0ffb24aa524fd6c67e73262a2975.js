f5148997f8dd0cc230d33f0b86080b071d2db2ab
Ovidiu Rugina <rovidiu@gmail.com>
2020-09-26 17:58:55 +0300

    Updates

---
+++ precache-manifest.4f1f0ffb24aa524fd6c67e73262a2975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32d09aa8e7f77ccae84fcf8986eb6c2",
+    "url": "/trial-assignment-upwork/trial-assignment/index.html"
+  },
+  {
+    "revision": "9d147a0ce070276ba60b",
+    "url": "/trial-assignment-upwork/trial-assignment/static/css/main.37944831.chunk.css"
+  },
+  {
+    "revision": "6a161fe1f8ccc8a0bfcb",
+    "url": "/trial-assignment-upwork/trial-assignment/static/js/2.0d969d4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trial-assignment-upwork/trial-assignment/static/js/2.0d969d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d147a0ce070276ba60b",
+    "url": "/trial-assignment-upwork/trial-assignment/static/js/main.0de71794.chunk.js"
+  },
+  {
+    "revision": "a575eb162c44f2989403",
+    "url": "/trial-assignment-upwork/trial-assignment/static/js/runtime-main.c02450f8.js"
+  },
+  {
+    "revision": "697eab48ddbb8f3429f07510634bf42e",
+    "url": "/trial-assignment-upwork/trial-assignment/static/media/close.697eab48.svg"
+  }
+]);
