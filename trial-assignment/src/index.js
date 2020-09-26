93821e45b8a5e3c8013e3521cb4a0ee9be3e16d5
Ovidiu Rugina <rovidiu@gmail.com>
2020-09-26 23:04:17 +0300

    Application code

--- trial-assignment/src/index.js
+++ trial-assignment/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider, updateIntl } from 'react-intl-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store'
+import NotificationsContainer from './components/notifications'
+import router from './routes/router'
+import browserHistory from './history'
+import languages from './languages'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const locale = 'en'
+
+store.dispatch(updateIntl({
+  locale,
+  messages: languages[locale],
+}))
+
+class App extends React.Component {
+  componentDidMount(){
+  }
+  
+  render() {
+    return (
+      <Provider
+        store={store}
+      >
+        <PersistGate loading={null} persistor={persistor}>
+          <div>
+            <NotificationsContainer />
+            {router(browserHistory)}
+          </div>
+        </PersistGate>
+      </Provider>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
